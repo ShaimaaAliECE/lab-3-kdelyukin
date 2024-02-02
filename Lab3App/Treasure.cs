0f6c364910f2c0d45b0ec8fe4756843eebf621ae
Kirill <kdelyuki@uwo.ca>
2024-02-02 18:20:45 -0500

    push2

---
+++ Lab3App/Treasure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    abstract class Treasure : Collectable
+    {
+        public int Score { get; set; }
+
+        public void UpdateTotalScore()
+        {
+            Board.TotalScore += Score;
+        }
+
+        // AddMe method is inherited from Collectable
+    }
+}
